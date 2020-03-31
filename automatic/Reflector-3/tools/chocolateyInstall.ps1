69ed76f5e6652b5a6da088f25c027feacceaac1b
meek2100 <dtheurer@live.com>
2020-03-31 16:52:38 -0600

    Add support to Reflector 3 package for 32 bit and 64 bit package downloads.

--- automatic/Reflector-3/tools/chocolateyInstall.ps1
+++ automatic/Reflector-3/tools/chocolateyInstall.ps1
@@ -1,17 +1,22 @@
 ﻿$ErrorActionPreference = 'Stop'
-$checksum = '9ee6ec4aa7a47e93bc4310cceba220cd2c082368178d105ba1ec7cef7881d180'
-$url = 'https://download.airsquirrels.com/Reflector3/Windows/Reflector3.2.0-64.msi'
+$checksum = '4bf4e01d45437e0971157cda4417d99583d544dcc77a7082ff49bd0b6ef56a61'
+$url = 'https://download.airsquirrels.com/Reflector3/Windows/Reflector3.2.0-32.msi'
+$checksum64 = '9ee6ec4aa7a47e93bc4310cceba220cd2c082368178d105ba1ec7cef7881d180'
+$url64 = 'https://download.airsquirrels.com/Reflector3/Windows/Reflector3.2.0-64.msi'
 
 $packageArgs = @{
   packageName    = 'reflector-3'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   fileType       = 'msi'
   url            = $url
+  url64bit       = $url64
   silentArgs     = '/quiet /norestart'
   validExitCodes = @(0)
   softwareName   = 'reflector-3*'
   checksum       = $checksum
   checksumType   = 'sha256'
+  checksum64     = $checksum64
+  checksumType64 = 'sha256'
 }
 
 Install-ChocolateyPackage @packageArgs
--- automatic/Reflector-3/update.ps1
+++ automatic/Reflector-3/update.ps1
@@ -3,13 +3,16 @@ import-module au
 
 $download_page_url = 'https://www.airsquirrels.com/reflector/release-notes/windows'
 $url_part1 = 'https://download.airsquirrels.com/Reflector3/Windows/Reflector'
-$url_part2 = '-64.msi'
+$url32_part2 = '-32.msi'
+$url64_part2 = '-64.msi'
 
 function global:au_SearchReplace {
     @{
         'tools\ChocolateyInstall.ps1' = @{
             "(^[$]checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
-            "(^[$]url\s*=\s*)('.*')"   = "`$1'$($Latest.Url)'"
+			"(^[$]checksum64\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum64)'"
+            "(^[$]url\s*=\s*)('.*')"   = "`$1'$($Latest.Url32)'"
+			"(^[$]url64\s*=\s*)('.*')"   = "`$1'$($Latest.Url64)'"
         }
      }
 }
@@ -20,11 +23,12 @@ function global:au_GetLatest {
      # Get Version
     $homepage_content -match 'Version \d+\.\d+\.\d+'| Out-Null
 	$version = $matches[0] -replace "Version ", ""
-    $url = $url_part1 + $version + $url_part2
+    $url32 = $url_part1 + $version + $url32_part2
+	$url64 = $url_part1 + $version + $url64_part2
     
 
-    $Latest = @{ URL = $url; Version = $version }
+    $Latest = @{ URL32 = $url32; URL64 = $url64; Version = $version }
     return $Latest
 }
 
-update -NoCheckUrl -ChecksumFor 32
+update -NoCheckChocoVersion
