3e7729efc4b553821481015a43c3271c41b97658
meek2100 <dtheurer@live.com>
2020-03-31 00:15:25 -0600

    Update package name.

--- automatic/AirParrot-2/tools/chocolateyInstall.ps1
+++ automatic/AirParrot-2/tools/chocolateyInstall.ps1
@@ -3,13 +3,13 @@ $checksum = 'e169fe8a22616b0588e05bcbbc0d7abd612cd1ae120b4c1c6b22fece0899a815'
 $url = 'https://akamaicdn.webex.com/client/WBXclient-39.7.7-27/webexapp.msi'
 
 $packageArgs = @{
-  packageName    = 'airparrot2'
+  packageName    = 'airparrot-2'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   fileType       = 'msi'
   url            = $url
   silentArgs     = '/quiet /norestart'
   validExitCodes = @(0)
-  softwareName   = 'airparrot2*'
+  softwareName   = 'airparrot-2*'
   checksum       = $checksum
   checksumType   = 'sha256'
 }
