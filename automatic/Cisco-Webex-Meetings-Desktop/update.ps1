47c4ec7b73340f83510babb68b6c1d4e35fa4937
meek2100 <dtheurer@live.com>
2020-03-30 18:17:42 -0600

    Initial Webex Chocolatey Script

---
+++ automatic/Cisco-Webex-Meetings-Desktop/update.ps1
@@ -0,0 +1,32 @@
+﻿$ErrorActionPreference = 'Stop'
+import-module au
+
+$download_page_url = 'https://control4.webex.com/webappng/sites/control4/dashboard/download'
+$url_part1 = 'https://akamaicdn.webex.com/client/'
+$url_part2 = '/webexapp.msi'
+
+function global:au_SearchReplace {
+    @{
+        'tools\ChocolateyInstall.ps1' = @{
+            "(^[$]checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+            "(^[$]url\s*=\s*)('.*')"   = "`$1'$($Latest.Url)'"
+        }
+     }
+}
+
+function global:au_GetLatest {
+    $homepage_content = Invoke-WebRequest -UseBasicParsing -Uri $download_page_url
+
+     # Get Version
+    $homepage_content -match 'WBXclient\-\d+\.\d+\.\d+\-\d+'| Out-Null
+    $url_version = $matches[0]
+	$recodeversion = $matches[0] -replace "WBXclient-", ""
+	$version = $recodeversion -replace "-", "."
+    $url = $url_part1 + $url_version + $url_part2
+    
+
+    $Latest = @{ URL = $url; Version = $version }
+    return $Latest
+}
+
+update -NoCheckUrl -ChecksumFor 32
