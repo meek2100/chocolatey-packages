091f50223b5c7bd755a59d001e81c0918de71d7e
meek2100 <dtheurer@live.com>
2020-03-31 13:07:38 -0600

    Initial release Outlook Google Calendar Sync script.

---
+++ automatic/Outlook-Google-Calendar-Sync/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop'
+$checksum = '63ebb1e82542fb0e4a6cab0d34f89bafff183930557d90963658b64c8f6cc290'
+$url = 'https://github.com/phw198/OutlookGoogleCalendarSync/releases/download/v2.8.0-beta/Setup.exe'
+
+$packageArgs = @{
+  packageName    = 'outlook-google-calendar-sync'
+  unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  fileType       = 'exe'
+  url            = $url
+  silentArgs     = '/quiet /norestart'
+  validExitCodes = @(0)
+  softwareName   = 'outlook-google-calendar-sync*'
+  checksum       = $checksum
+  checksumType   = 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/Outlook-Google-Calendar-Sync/update.ps1
@@ -0,0 +1,32 @@
+﻿$ErrorActionPreference = 'Stop'
+import-module au
+
+$download_page_url = 'https://github.com/phw198/OutlookGoogleCalendarSync/releases'
+$url_part1 = 'https://github.com/phw198/OutlookGoogleCalendarSync/releases/download/'
+$url_part2 = '/Setup.exe'
+
+function global:au_SearchReplace {
+    @{
+        'tools\ChocolateyInstall.ps1' = @{
+            "(^[$]checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+            "(^[$]url\s*=\s*)('.*')"   = "`$1'$($Latest.Url)'"
+        }
+     }
+}
+
+function global:au_GetLatest {
+    $homepage_content = Invoke-WebRequest -UseBasicParsing -Uri $download_page_url
+
+     # Get Version
+    $homepage_content -match 'v\d+\.\d+\.\d+\-beta'| Out-Null
+    $url_version = $matches[0] 
+	$recode_version = $url_version -replace "v", ""
+	$version = $recode_version -replace "-beta", ""
+    $url = $url_part1 + $url_version + $url_part2
+    
+
+    $Latest = @{ URL = $url; Version = $version }
+    return $Latest
+}
+
+update -NoCheckUrl -ChecksumFor 32
