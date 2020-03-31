f0c21fbff11e2dc43a3ad5c8a2dc52a185867d51
meek2100 <dtheurer@live.com>
2020-03-31 17:01:14 -0600

    Add support to AirParrot 2 package for 32 bit and 64 bit package downloads.

--- automatic/AirParrot-2/tools/chocolateyInstall.ps1
+++ automatic/AirParrot-2/tools/chocolateyInstall.ps1
@@ -1,17 +1,22 @@
 ﻿$ErrorActionPreference = 'Stop'
-$checksum = '13c785ec471db1a83e8056869a16cd7a38b36159e88e2770718753d19d994bab'
-$url = 'https://download.airsquirrels.com/AirParrot2/Windows/AirParrot2.7.5-64.msi'
+$checksum = '4bf4e01d45437e0971157cda4417d99583d544dcc77a7082ff49bd0b6ef56a61'
+$url = 'https://download.airsquirrels.com/Reflector3/Windows/Reflector3.2.0-32.msi'
+$checksum64 = '9ee6ec4aa7a47e93bc4310cceba220cd2c082368178d105ba1ec7cef7881d180'
+$url64 = 'https://download.airsquirrels.com/Reflector3/Windows/Reflector3.2.0-64.msi'
 
 $packageArgs = @{
   packageName    = 'airparrot-2'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   fileType       = 'msi'
   url            = $url
+  url64bit       = $url64
   silentArgs     = '/quiet /norestart'
   validExitCodes = @(0)
   softwareName   = 'airparrot-2*'
   checksum       = $checksum
   checksumType   = 'sha256'
+  checksum64     = $checksum64
+  checksumType64 = 'sha256'
 }
 
 Install-ChocolateyPackage @packageArgs
