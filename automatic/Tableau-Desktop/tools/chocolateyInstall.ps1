0e767733760e8b4f4f32139e9d3e10507445c5e5
meek2100 <dtheurer@live.com>
2020-03-31 11:58:07 -0600

    Initial release for Tableau Desktop script.

---
+++ automatic/Tableau-Desktop/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop'
+$checksum = '8ea7f27447b99dc754cb99dd1507bb293cffca9ff8890729186a5ab96ac4a455'
+$url = 'https://downloads.tableau.com/tssoftware/TableauDesktop-64bit-2020-1-2.exe'
+
+$packageArgs = @{
+  packageName    = 'tableau-desktop'
+  unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  fileType       = 'exe'
+  url            = $url
+  silentArgs     = '/quiet /norestart'
+  validExitCodes = @(0)
+  softwareName   = 'tableau-desktop*'
+  checksum       = $checksum
+  checksumType   = 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/Tableau-Desktop/update.ps1
@@ -0,0 +1,31 @@
+﻿$ErrorActionPreference = 'Stop'
+import-module au
+
+$download_page_url = 'https://www.tableau.com/support/releases'
+$url_part1 = 'https://downloads.tableau.com/tssoftware/TableauDesktop-64bit-'
+$url_part2 = '.exe'
+
+function global:au_SearchReplace {
+    @{
+        'tools\ChocolateyInstall.ps1' = @{
+            "(^[$]checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+            "(^[$]url\s*=\s*)('.*')"   = "`$1'$($Latest.Url)'"
+        }
+     }
+}
+
+function global:au_GetLatest {
+    $homepage_content = Invoke-WebRequest -UseBasicParsing -Uri $download_page_url
+
+     # Get Version
+    $homepage_content -match '\/\d+\.\d+\.\d+'| Out-Null
+    $version = $matches[0] -replace "/", ""
+	$url_version = $version -replace "\.", "-"
+    $url = $url_part1 + $url_version + $url_part2
+    
+
+    $Latest = @{ URL = $url; Version = $version }
+    return $Latest
+}
+
+update -NoCheckUrl -ChecksumFor 32
