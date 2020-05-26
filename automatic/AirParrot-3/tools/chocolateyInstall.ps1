cc5b465ac1a81d843d450c94bd200e82de30cea8
meek2100 <dtheurer@live.com>
2020-05-26 10:08:02 -0600

    AirParrot 3 Inital Release

---
+++ automatic/AirParrot-3/tools/chocolateyInstall.ps1
@@ -0,0 +1,22 @@
+﻿$ErrorActionPreference = 'Stop'
+$checksum = '6410c1b8710913740e8a5e07cd0178abb6546138850949f0e29e0ef28c9c62a0'
+$url = 'https://download.airsquirrels.com/AirParrot2/Windows/AirParrot2.7.5-32.msi'
+$checksum64 = '13c785ec471db1a83e8056869a16cd7a38b36159e88e2770718753d19d994bab'
+$url64 = 'https://download.airsquirrels.com/AirParrot2/Windows/AirParrot2.7.5-64.msi'
+
+$packageArgs = @{
+  packageName    = 'airparrot-3'
+  unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  fileType       = 'msi'
+  url            = $url
+  url64bit       = $url64
+  silentArgs     = '/quiet /norestart'
+  validExitCodes = @(0)
+  softwareName   = 'airparrot-3*'
+  checksum       = $checksum
+  checksumType   = 'sha256'
+  checksum64     = $checksum64
+  checksumType64 = 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
