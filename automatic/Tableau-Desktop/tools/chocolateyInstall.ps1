59261034c93653c48f1318d1d6db39c3ece7c9ba
Chocolatey <chocolatey@realdimensions.net>
2020-06-24 00:06:16 +0000

    AU: 1 updated - Tableau-Desktop [skip ci] https://gist.github.com/d1727a692d06b4b337db230100c9c2f8/5d70485ff9cb8e4de395bd0ffbf8d0769fa40faf

--- automatic/Tableau-Desktop/tools/chocolateyInstall.ps1
+++ automatic/Tableau-Desktop/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
-$checksum = '07524c3e94e12a9bbd0d17ad26faffc87c06545ec1b528687af643a8bac68ce6'
-$url = 'https://downloads.tableau.com/tssoftware/TableauDesktop-64bit-2020-2-1.exe'
+$checksum = '5c399a73645f5137159cba55d9609eecb1fd405a0d2cc73f060ee6978cbbafb4'
+$url = 'https://downloads.tableau.com/tssoftware/TableauDesktop-64bit-2020-2-2.exe'
 
 $packageArgs = @{
   packageName    = 'Tableau-Desktop'
