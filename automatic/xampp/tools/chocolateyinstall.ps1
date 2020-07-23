e17ae89e1b1856318e5f7b20410b9d8a41a2f9e6
meek2100 <dtheurer@live.com>
2020-07-23 11:05:40 -0600

    Intial commit for XAMPP

---
+++ automatic/xampp/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+$checksum = '6537ff078425dbab04278a1c3bbc457abc78c27c895f1b5d88b04f49f2fbfa52'
+$url = 'https://www.apachefriends.org/xampp-files/7.4.6/xampp-windows-x64-7.4.6-0-VC15-installer.exe'
+
+$packageArgs = @{
+  packageName    = 'Bitnami-XAMPP'
+  unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  fileType       = 'exe'
+  url            = $url
+  silentArgs     = "--mode unattended"
+  validExitCodes = @(0, 3010, 1641)
+  softwareName   = 'xampp*'
+  checksum       = $checksum
+  checksumType   = 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/xampp/update.ps1
@@ -0,0 +1,32 @@
+﻿$ErrorActionPreference = 'Stop'
+import-module au
+
+$download_page_url = 'https://www.apachefriends.org/blog.html'
+$url_part1 = 'https://www.apachefriends.org/xampp-files/'
+$url_part2 = '/xampp-windows-x64-'
+$url_part3 = '-VC15-installer.exe'
+
+function global:au_SearchReplace {
+    @{
+        'tools\ChocolateyInstall.ps1' = @{
+            "(^[$]checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+            "(^[$]url\s*=\s*)('.*')"   = "`$1'$($Latest.Url)'"
+        }
+     }
+}
+
+function global:au_GetLatest {
+    $homepage_content = Invoke-WebRequest -UseBasicParsing -Uri $download_page_url
+
+     # Get Version
+    $homepage_content -match '\d{1}\.4\.\d{1,2}\-0'| Out-Null
+    $real_version = $matches[0]
+	$version = $real_version -replace "-0", ""
+	$url = $url_part1 + $version + $url_part2 + $real_version + $url_part3
+    
+
+    $Latest = @{ URL = $url; Version = $version }
+    return $Latest
+}
+
+update
