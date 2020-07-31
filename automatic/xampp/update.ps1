d0021ea96e2f6fae24c33420ffbd48072f2f5216
meek2100 <dtheurer@live.com>
2020-07-30 21:36:55 -0600

    Update Xampp to use sourceforge mirror URL to resolve TLS error.

--- automatic/xampp/update.ps1
+++ automatic/xampp/update.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 import-module au
 
-$download_page_url = 'https://www.apachefriends.org/blog.html'
-$url_part1 = 'https://www.apachefriends.org/xampp-files/'
+$download_page_url = 'https://sourceforge.net/projects/xampp/files/XAMPP%20Windows/'
+$url_part1 = 'https://sourceforge.net/projects/xampp/files/XAMPP%20Windows/'
 $url_part2 = '/xampp-windows-x64-'
-$url_part3 = '-VC15-installer.exe'
+$url_part3 = '-0-VC15-installer.exe'
 
 function global:au_SearchReplace {
     @{
@@ -19,10 +19,9 @@ function global:au_GetLatest {
     $homepage_content = Invoke-WebRequest -UseBasicParsing -Uri $download_page_url
 
      # Get Version
-    $homepage_content -match '\d{1}\.4\.\d{1,2}\-0'| Out-Null
-    $real_version = $matches[0]
-	$version = $real_version -replace "-0", ""
-	$url = $url_part1 + $version + $url_part2 + $real_version + $url_part3
+    $homepage_content -match '\d{1}\.4\.\d{1,2}'| Out-Null
+    $version = $matches[0]
+	$url = $url_part1 + $version + $url_part2 + $version + $url_part3
     
 
     $Latest = @{ URL = $url; Version = $version }
