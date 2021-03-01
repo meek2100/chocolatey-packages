19e8f302f903ad1662b901bb55c652024e6f972c
meek2100 <dtheurer@live.com>
2021-03-01 13:04:08 -0700

    Initial commit of Tableau Server package.

---
+++ automatic/Tableau-Server/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop'
+$checksum = '79b7fbcbec7289f2141e933259a9ad87be9ad6298262e9d4d118dce58c0a7a72'
+$url = 'https://downloads.tableau.com/tssoftware/TableauServer-64bit-2020-4-2.exe'
+
+$packageArgs = @{
+  packageName    = 'Tableau-Server'
+  unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  fileType       = 'exe'
+  url            = $url
+  silentArgs     = '/quiet /norestart ACCEPTEULA=1'
+  validExitCodes = @(0)
+  softwareName   = 'Tableau*'
+  checksum       = $checksum
+  checksumType   = 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
