7da37328836f6009a5a307cfc8123b22daccea45
teknowledgist <teknowledgist@users.noreply.github.com>
2021-05-03 13:37:12 -0400

    enhance tableau-desktop info
    There is important information to share about installing the Tableau Desktop package that might not make a difference for a single user, but will make a big difference for a large deployment with updates.
    
    Also added installer logging and a "reboot required" error code.

--- automatic/Tableau-Desktop/tools/chocolateyInstall.ps1
+++ automatic/Tableau-Desktop/tools/chocolateyInstall.ps1
@@ -6,11 +6,11 @@ $packageArgs = @{
   packageName    = 'Tableau-Desktop'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   fileType       = 'exe'
-  url            = $url
-  silentArgs     = '/quiet /norestart ACCEPTEULA=1'
-  validExitCodes = @(0)
+  url64bit       = $url
+  silentArgs     = "/quiet /norestart /LOG `"$($env:TEMP)\Tableau-$($env:chocolateyPackageVersion)-Install.log`" ACCEPTEULA=1"
+  validExitCodes = @(0,3010)
   softwareName   = 'Tableau*'
-  checksum       = $checksum
+  checksum64     = $checksum
   checksumType   = 'sha256'
 }
 
