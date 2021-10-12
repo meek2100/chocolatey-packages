6ab0ae9eb20367afea2c9409632023dcf1aa8037
meek2100 <dtheurer@live.com>
2021-10-12 00:04:35 -0700

    Initial commit of EdgeDeflector package.

---
+++ automatic/EdgeDeflector/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop'
+$checksum = 'df81b4af738b4222662e290ace98503121d99e204cba4834effb29f1c0fd4afc'
+$url = 'https://github.com/da2x/EdgeDeflector/releases/download/v1.2.3.0/EdgeDeflector_install.exe'
+
+$packageArgs = @{
+  packageName    = 'EdgeDeflector'
+  unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  fileType       = 'exe'
+  url            = $url
+  silentArgs     = '/quiet /norestart'
+  validExitCodes = @(0)
+  softwareName   = 'EdgeDeflector*'
+  checksum       = $checksum
+  checksumType   = 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/EdgeDeflector/update.ps1
@@ -0,0 +1,30 @@
+﻿$ErrorActionPreference = 'Stop'
+import-module au
+
+$download_page_url = 'https://github.com/da2x/EdgeDeflector/releases'
+$url_part1 = 'https://github.com/da2x/EdgeDeflector/releases/download/'
+$url_part2 = '/EdgeDeflector_install.exe'
+
+function global:au_SearchReplace {
+    @{
+        'tools\ChocolateyInstall.ps1' = @{
+            "(^[$]checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+            "(^[$]url\s*=\s*)('.*')"   = "`$1'$($Latest.Url)'"
+        }
+     }
+}
+
+function global:au_GetLatest {
+    $homepage_content = Invoke-WebRequest -UseBasicParsing -Uri $download_page_url
+
+     # Get Version
+    $homepage_content -match 'v\d+\.\d+\.\d+.\d+'| Out-Null
+    $url_version = $matches[0] 
+    $url = $url_part1 + $url_version + $url_part2
+    
+
+    $Latest = @{ URL = $url; Version = $version }
+    return $Latest
+}
+
+update
