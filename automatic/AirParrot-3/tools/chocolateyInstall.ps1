2c6ebf4febc9beefd5098864d2ad3d1441c64f14
Chocolatey <chocolatey@realdimensions.net>
2021-12-08 00:06:46 +0000

    AU: 1 updated - AirParrot-3 [skip ci] https://gist.github.com/d1727a692d06b4b337db230100c9c2f8/4d5f706a92c74135da3605d7617604b7115bcef7

--- automatic/AirParrot-3/tools/chocolateyInstall.ps1
+++ automatic/AirParrot-3/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
-$checksum = '445958796c912f4c4e26bc780630c863374363a5684d15c1969d4831c28da4e3'
-$url = 'https://download.airsquirrels.com/AirParrot3/Windows/AirParrot-3.1.3-32.msi'
-$checksum64 = '8b1063fe3c6f45477360413078249215bee367e068af55181eea0b3a8d55aeb6'
-$url64 = 'https://download.airsquirrels.com/AirParrot3/Windows/AirParrot-3.1.3-64.msi'
+$checksum = 'de2bf68e092eb3230ff2888e7d954c385fd4e2d1242bdb96c70f861f19fdbc3f'
+$url = 'https://download.airsquirrels.com/AirParrot3/Windows/AirParrot-3.1.4-32.msi'
+$checksum64 = 'ee956c18f6d4152648dd61e4eebd13677457269cacb5bc39505f33e61fd1936b'
+$url64 = 'https://download.airsquirrels.com/AirParrot3/Windows/AirParrot-3.1.4-64.msi'
 
 $packageArgs = @{
   packageName    = 'AirParrot-3'
