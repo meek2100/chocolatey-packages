37f4cbf839f3ebfe722479f1e7aa45edccdd4625
meek2100 <dtheurer@live.com>
2022-04-15 16:06:38 -0700

    Initial commit of Sourcetree

---
+++ automatic/sourcetree/tools/chocolateyInstall.ps1
@@ -0,0 +1,26 @@
+﻿$ErrorActionPreference = 'Stop'
+
+# Check if Sourcetree standard (with Squirrel installer) is installed 
+[array] $key = Get-UninstallRegistryKey "sourcetree" | Where-Object { -Not ($_.WindowsInstaller) }
+if ($key.Count -gt 0) {
+  Write-Warning "Found installation of standard version of Sourcetree."
+  Write-Warning "This package will install the enterprise version of Sourcetree."
+  Write-Warning "Both applications can be installed side-by-side. Settings won't be migrated from the existing installation. If you no longer want the standard version installed you can uninstall it from Windows control panel."
+}
+
+# Install Sourcetree Enterprise
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  softwareName  = 'Sourcetree*'
+  fileType      = 'msi'
+  silentArgs    = "/qn /norestart ACCEPTEULA=1 /l*v `"$env:TEMP\$env:ChocolateyPackageName.$env:ChocolateyPackageVersion.log`""
+  validExitCodes= @(0,1641,3010)
+  url           = 'https://product-downloads.atlassian.com/software/sourcetree/windows/ga/SourcetreeEnterpriseSetup_3.4.1.msi'
+  checksum      = '6720d2278c309d0fb9117451cf92ed2af25a26588fbd39a08da5266578004e74'
+  checksumType  = 'sha256'
+  url64bit      = ''
+  checksum64    = ''
+  checksumType64= 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/sourcetree/update.ps1
@@ -0,0 +1,36 @@
+﻿$ErrorActionPreference = 'Stop'
+import-module au
+
+$download_page_url = 'https://www.sourcetreeapp.com/download-archives'
+$url_part1 = 'https://product-downloads.atlassian.com/software/sourcetree/windows/ga/SourceTreeSetup-'
+$url_part2 = '.exe'
+$release_notes_part1 = 'https://product-downloads.atlassian.com/software/sourcetree/windows/ga/ReleaseNotes_'
+$release_notes_part2 = '.html'
+
+function global:au_SearchReplace {
+    @{
+        'tools\ChocolateyInstall.ps1' = @{
+            "(^[$]checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+            "(^[$]url\s*=\s*)('.*')"   = "`$1'$($Latest.Url)'"
+        }
+		'sourcetree.nuspec' = @{
+            "(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`$2"
+        }
+     }
+}
+
+function global:au_GetLatest {
+    $homepage_content = Invoke-WebRequest -UseBasicParsing -Uri $download_page_url
+
+     # Get Version
+    $homepage_content -match 'https://product-downloads.atlassian.com/software/sourcetree/windows/ga/SourceTreeSetup-\d+\.\d+\.\d+'| Out-Null
+	$version = $matches[0] -replace "https://product-downloads.atlassian.com/software/sourcetree/windows/ga/SourceTreeSetup-", ""
+    $url = $url_part1 + $version + $url_part2
+	$release_notes = $release_notes_part1 + $version + $release_notes_part2
+    
+
+    $Latest = @{ URL = $url; Version = $version; ReleaseNotes = $release_notes }
+    return $Latest
+}
+
+update
