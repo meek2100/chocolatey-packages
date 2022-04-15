1bf0f9e472925d476bbb85cf1986263fe3a34607
meek2100 <dtheurer@live.com>
2022-04-14 20:40:51 -0700

    Initial Commit XAMPP 8.1

---
+++ automatic/XAMPP-81/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+$checksum = '8f198515f346b362092febe2953583ead98430462445ae7be1514eaf4ddb5bea'
+$url = 'https://sourceforge.net/projects/xampp/files/XAMPP%20Windows/8.0.17/xampp-windows-x64-8.0.17-0-VS16-installer.exe'
+
+$packageArgs = @{
+  packageName    = 'XAMPP-81'
+  unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  fileType       = 'exe'
+  url            = $url
+  silentArgs     = "--mode unattended --launchapps 0"
+  validExitCodes = @(0, 3010, 1641)
+  softwareName   = 'xampp*'
+  checksum       = $checksum
+  checksumType   = 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
