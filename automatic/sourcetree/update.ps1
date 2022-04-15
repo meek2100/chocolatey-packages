5e6bdc2f0fc880d57cd9ee08828b5bce48ebe97e
meek2100 <dtheurer@live.com>
2022-04-15 16:26:44 -0700

    Fix SourceTree so it uses Enterprise link

--- automatic/sourcetree/update.ps1
+++ automatic/sourcetree/update.ps1
@@ -2,8 +2,8 @@
 import-module au
 
 $download_page_url = 'https://www.sourcetreeapp.com/download-archives'
-$url_part1 = 'https://product-downloads.atlassian.com/software/sourcetree/windows/ga/SourceTreeSetup-'
-$url_part2 = '.exe'
+$url_part1 = 'https://product-downloads.atlassian.com/software/sourcetree/windows/ga/SourcetreeEnterpriseSetup_'
+$url_part2 = '.msi'
 $release_notes_part1 = 'https://product-downloads.atlassian.com/software/sourcetree/windows/ga/ReleaseNotes_'
 $release_notes_part2 = '.html'
 
@@ -23,8 +23,8 @@ function global:au_GetLatest {
     $homepage_content = Invoke-WebRequest -UseBasicParsing -Uri $download_page_url
 
      # Get Version
-    $homepage_content -match 'https://product-downloads.atlassian.com/software/sourcetree/windows/ga/SourceTreeSetup-\d+\.\d+\.\d+'| Out-Null
-	$version = $matches[0] -replace "https://product-downloads.atlassian.com/software/sourcetree/windows/ga/SourceTreeSetup-", ""
+    $homepage_content -match 'https://product-downloads.atlassian.com/software/sourcetree/windows/ga/SourcetreeEnterpriseSetup_\d+\.\d+\.\d+'| Out-Null
+	$version = $matches[0] -replace "https://product-downloads.atlassian.com/software/sourcetree/windows/ga/SourcetreeEnterpriseSetup_", ""
     $url = $url_part1 + $version + $url_part2
 	$release_notes = $release_notes_part1 + $version + $release_notes_part2
     
