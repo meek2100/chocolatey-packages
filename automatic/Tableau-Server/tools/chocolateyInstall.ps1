d84a3ba5df953a3a12ebe5b20507f73c95d86002
Chocolatey <chocolatey@realdimensions.net>
2022-04-23 08:09:59 +0000

    AU: 1 updated - Tableau-Server [skip ci] https://gist.github.com/d1727a692d06b4b337db230100c9c2f8/6fa880efa53c2095aadc4077fb655dd467704be1

--- automatic/Tableau-Server/tools/chocolateyInstall.ps1
+++ automatic/Tableau-Server/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'
-$checksum = '43d548509ab506f5dc0732e14fe49116230b24b60796f07d0cb0cce42f5f98b0'
-$url = 'https://downloads.tableau.com/tssoftware/TableauServer-64bit-2022-1-0.exe'
+$checksum = '379599715dac378f1d026a7e354552380720819d92c90c6e85648d4bd05b749f'
+$url = 'https://downloads.tableau.com/tssoftware/TableauServer-64bit-2022-1-1.exe'
 
 $packageArgs = @{
   packageName    = 'Tableau-Server'
