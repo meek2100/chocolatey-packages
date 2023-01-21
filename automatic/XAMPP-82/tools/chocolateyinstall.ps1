29957e83e3ad4ca348c7b25c8f01c023d98aab74
meek2100 <dtheurer@live.com>
2023-01-20 21:02:03 -0800

    Initial commit for XAMPP 8.2.0 package

---
+++ automatic/XAMPP-82/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+$checksum = 'ead3846e8fc6869463fe0dc8838155b9f8351f66bcc8ee426f156dcc9aa43667'
+$url = 'https://sourceforge.net/projects/xampp/files/XAMPP%20Windows/8.1.0/xampp-windows-x64-8.2.0-0-VS16-installer.exe'
+
+$packageArgs = @{
+  packageName    = 'XAMPP-82'
+  unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  fileType       = 'exe'
+  url            = $url
+  silentArgs     = "--mode unattended --launchapps 0"
+  validExitCodes = @(0, 3010, 1641)
+  softwareName   = 'xampp*'
+  checksum       = $checksum
+  checksumType   = 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
