6ae2b47754aa7abc35dded82a46213fd528bc455
Chocolatey <chocolatey@realdimensions.net>
2023-02-27 16:06:16 +0000

    AU: 1 updated - Zoom-Outlook [skip ci] https://gist.github.com/d1727a692d06b4b337db230100c9c2f8/ede6e4972d249bced59368ddea43c5b15a6b41a1

--- automatic/Zoom-Outlook/tools/chocolateyInstall.ps1
+++ automatic/Zoom-Outlook/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
  
-$checksum = '4801f112df14d17303e676eebcdd023a967f1041d76dedf4881111c6a0cd4e4b'
-$url = 'https://zoom.us/client/5.13.5.783/ZoomOutlookPluginSetup.msi'
+$checksum = 'c1c3829bdfa93e3fdd83fa8e317bd653617b5978e9ee6be5a1bba66a9d9452f2'
+$url = 'https://zoom.us/client/5.13.10.810/ZoomOutlookPluginSetup.msi'
 
 $packageArgs = @{
   packageName    = 'Zoom-Outlook'
